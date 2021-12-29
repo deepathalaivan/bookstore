1fd8cfd165e9110f6ab060171f42abe0d2edd249
Deepa Kalyanasundaram <deepathalaivan@gmail.com>
2021-12-29 16:35:38 -0600

    Bookstore mysql database version

--- src/main/java/com/example/bookstore/model/Book.java
+++ src/main/java/com/example/bookstore/model/Book.java
@@ -1,45 +1,21 @@
 package com.example.bookstore.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 public class Book {
     @Id
     private Long bookId;
     private String author;
     private String bookName;
-
-    public Book() {
-    }
-
-    public Book(Long bookId, String author, String bookName) {
-        this.bookId = bookId;
-        this.author = author;
-        this.bookName = bookName;
-    }
-
-    public Long getBookId() {
-        return bookId;
-    }
-
-    public void setBookId(Long bookId) {
-        this.bookId = bookId;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getBookName() {
-        return bookName;
-    }
-
-    public void setBookName(String bookName) {
-        this.bookName = bookName;
-    }
 }
